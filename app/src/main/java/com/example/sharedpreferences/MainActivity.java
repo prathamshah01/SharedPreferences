d0ec8efc8468521d242a69dbc786ba61cb4162b9
prathamshah01 <imprathamshah9@gmail.com>
2023-03-17 17:11:45 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.sharedpreferences;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.sharedpreferences.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openNewActivity();
+
+            }
+        });
+    }
+
+    public void openNewActivity(){
+
+        Intent intent= new Intent(this,RegisterActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
